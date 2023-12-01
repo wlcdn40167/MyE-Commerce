ab442b5d1dc7d9cc20b01fcc2a0ddada296a7d6a
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-01 23:32:13 +0800

    Day 1.5

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,6 +1,62 @@
 import React from "react";
-
+import "./navbar.css";
+import logo from "../assets/logo.png";
+import cart_icon from "../assets/cart_icon.png";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <div></div>;
+  const [menu, setMenu] = useState("shop");
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} />
+        <p>Shop</p>
+      </div>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link to="/">Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          <Link to="/mens">Men</Link>
+          {menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <Link to="/womens">Women</Link>
+          {menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link to="/kids">Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
