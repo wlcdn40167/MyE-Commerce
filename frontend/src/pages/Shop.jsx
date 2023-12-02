5126cc14e7c7ec19e9218ffaac8ec99b881e3a09
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-02 23:29:39 +0800

    day 2.5

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Popular from "../components/popular/Popular";
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   );
 };
