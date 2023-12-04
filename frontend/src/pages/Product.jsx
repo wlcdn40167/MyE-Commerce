7fb8640f28c4ff3978180755bac0f9ed823fc7d5
wlcdn40167 <wlcdn40167@gmail.com>
2023-12-04 23:35:18 +0800

    day 5

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import Breadcrum from "../components/Breadcrums/Breadcrum";
+import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 export default Product;
